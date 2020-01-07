8515630816ebe5f06bec3a4b12a261252442b2b2
Arina <arina435@mail.ru>
2020-01-07 14:16:12 +0000

    price change back to normal

--- app.js
+++ app.js
@@ -102,9 +102,9 @@ app.get("/warning3", isLoggedIn, (req,res) =>{
 app.post("/success", async (req,res) =>{
 	console.log(req);
 	var d = new Date();
-	if(req.body.withdraw_amount == "2.00"){
+	if(req.body.withdraw_amount == "299.00"){
 		d.setDate(d.getDate()+30);
-	}else if(req.body.withdraw_amount == "3.00"){
+	}else if(req.body.withdraw_amount == "750.00"){
 		d.setDate(d.getDate()+90);
 	}else{
 		d.setDate(d.getDate()+180);
